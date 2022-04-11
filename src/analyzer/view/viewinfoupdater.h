e92c3c8248695d6b9fd2a7ed58233aebd10c9285
sanlav89 <sanlav89@mail.ru>
2022-04-11 23:28:53 +0300

    add app analyzer

--- src/analyzer/view/viewinfoupdater.h
+++ src/analyzer/view/viewinfoupdater.h
@@ -1,19 +1,17 @@
 #pragma once
 
-#include "utils/types.h"
+#include <utils/types.h>
 
 namespace analyzer {
 namespace view {
-
-using namespace utils;
 
 class ViewInfoUpdater
 {
 public:
     virtual ~ViewInfoUpdater() = default;
-    virtual void updateSpectrum(const spectrum_t &spectrum) = 0;
-    virtual void updateNuclides(const nuclides_t &nuclides) = 0;
-    virtual void updateEnergyScale(const poly_t &enpoly) = 0;
+    virtual void updateSpectrum(const utils::spectrum_t &spectrum) = 0;
+    virtual void updateNuclides(const utils::nuclides_t &nuclides) = 0;
+    virtual void updateEnergyScale(const utils::poly_t &enpoly) = 0;
     virtual void updateStatusMsg(const std::string &msg) = 0;
 };
 
