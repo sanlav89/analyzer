39d3a8efc7dbfc4fe85c7be29732a0dcf62c9c5f
sanlav89 <sanlav89@mail.ru>
2022-04-12 23:29:59 +0300

    refactoring done

--- matlab/f_LoadLsrmSpeFile.m
+++
@@ -1,7 +0,0 @@
-function [N] = f_LoadLsrmSpeFile(filename, size)
-N_COLS = size / 4;
-fid = fopen(filename, 'r');
-N = double(fread(fid,[N_COLS,Inf],'uint32'))';
-fclose(fid);
-end
-
--- matlab/f_gaussian.m
+++
@@ -1,6 +0,0 @@
-function [y] = f_gaussian(x, x0, sigma, S)
-
-y = S / (sigma * sqrt(2 * pi)) * exp(-(x - x0).^2 / (2 * sigma^2));
-
-end
-
